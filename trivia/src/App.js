c399c687ed0e6a9ae70855efde1798972a7b144b
sarahartsoe <sarahartsoe@gmail.com>
2021-07-13 12:25:19 -0400

    added bulm css

--- trivia/src/App.js
+++ trivia/src/App.js
@@ -12,18 +12,20 @@ function App () {
 
   return (
     <div className="container">
-      <div className="App">
+      <div className="App" class="notification is-black">
         <h1>Trivia</h1>
       </div>
       <div>
         <h1>Categories:</h1>
-        {categories.map(category => {
-          return (
-            <div key={category.category}>
-              <p>{category.category}</p>
-            </div>
-          )
-        })}
+        <div className="is-flex is-flex-direction-row is-flex-wrap-wrap is-align-content-space-between">
+          {categories.map(category => {
+            return (
+              <div key={category.category} class="card" class="tile is-parent is-6">
+                <p class="card-content" class="title has-text-centered tile is-child box">{category.category}</p>
+              </div>
+            )
+          })}
+        </div>
       </div>
     </div>
   )
