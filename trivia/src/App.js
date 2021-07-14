896f7ab005f3d4ab74c1967c063551d9211bc762
sarahartsoe <sarahartsoe@gmail.com>
2021-07-14 12:46:32 -0400

    moved category list into components folder

--- trivia/src/App.js
+++ trivia/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import './App.css'
 import { getCategoryList } from './api'
 import { Header } from './components/Header'
+import { CategoryList } from './components/CategoryList'
 
 function App () {
   const [categories, setCategories] = useState([])
@@ -14,15 +15,9 @@ function App () {
     <div>
       <Header />
       <h1>Categories:</h1>
-      <div className="is-flex is-flex-direction-row is-flex-wrap-wrap is-align-content-space-between">          {categories.map(category => {
-        return (
-          <div key={category.id} class="card" class="tile is-parent is-6">                <p class="card-content" class="title has-text-centered tile is-child box">{category.name}
-            <br></br>
-            <a class="button is-primary is-outlined">Play</a></p>
-            </div>
-        )
-      })}
-      </div>
+      <section>
+        <CategoryList categories={categories} />
+      </section>
     </div>
   )
 }
---
+++ trivia/src/components/CategoryList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const CategoryList = (props) => {
+  const { categories, setCategories } = props
+  return (
+    <div className="is-flex is-flex-direction-row is-flex-wrap-wrap is-align-content-space-between">
+      {categories.map(category => {
+        return (
+          <div key={category.id} class="card" class="tile is-parent is-6">                
+            <p class="card-content" class="title has-text-centered tile is-child box">{category.name}
+              <br></br>
+              <a class="button is-primary is-outlined">Play</a></p>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
